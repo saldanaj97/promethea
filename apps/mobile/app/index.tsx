2c74f5ea5557189c2991255fe084b64a39f9d71f
saldanaj97 <saldanaj97@gmail.com>
2025-08-22 22:23:13 -0500

    first commit

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
 import { Button } from "@repo/ui";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Native() {
   return (
